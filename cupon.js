c137f61dc4728a0a2a8d1c1501589f6d58b6b629
yisuscoin <r.alfaro1882@gmail.com>
2022-10-26 12:38:19 -0500

    agregando codigo html y js

---
+++ cupon.js
@@ -0,0 +1,30 @@
+const inputPrice = document.querySelector('#price');
+const inputCoupon = document.querySelector('#coupon');
+const btn = document.querySelector('#calcular');
+const pResult = document.querySelector('#result');
+
+class Coupon {
+    constructor(name, discount) {
+        this.name = name,
+        this.discount = discount
+    }
+}
+
+const coupons = [];
+
+coupons.push(new Coupon('D30', 30), new Coupon('D25', 25), new Coupon('D10', 10));
+
+function calcularPrecioConDescuento() {
+
+    const cuponValido = coupons.find(item => item.name == inputCoupon.value);
+
+    if (cuponValido != undefined) {
+        const precioConDescuento = Number((price.value)*(100 - cuponValido.discount) / 100);
+        pResult.innerText = `El precio con descuneto aplicado es de: ${precioConDescuento}`;
+        console.log(precioConDescuento);
+        
+    } else {
+        pResult.innerText = 'Descuento no Válido';
+        return
+    }
+}
